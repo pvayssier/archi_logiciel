76bc4899cd2cfb6c726b13aa40d180ff2b20b1cf
leo-grandmorcel <leo.grandmorcel@gmail.com>
2025-06-19 15:38:13 +0200

    feat: Add rover map

---
+++ src/broker/main.ts
@@ -0,0 +1,17 @@
+import { MqttBroker } from "./mqtt-broker";
+import { CommandRover } from "@model";
+import { EtatRover } from "@model";
+
+// Example usage:
+
+const brokerUrl = "mqtt://127.0.0.1:1883"; // Change this to your MQTT broker URL
+const mqttBroker = new MqttBroker(brokerUrl, "test");
+mqttBroker.subscribeToResponses((etat: EtatRover) => {
+  console.log("Rover state:", etat);
+});
+
+mqttBroker.publishCommand([
+  CommandRover.FORWARD,
+  CommandRover.FORWARD,
+  CommandRover.FORWARD,
+]);
